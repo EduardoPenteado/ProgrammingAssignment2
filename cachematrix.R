000ac6e34bc069f81ffb7df605eb6027b5661c12
Eduardo Penteado <41272789+EduardoPenteado@users.noreply.github.com>
2021-03-19 20:27:11 -0300

    Update cachematrix.R
    Second Submit

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,35 @@
 
-#This function cache the matrix
+#This function cache the matrix in a list
 makeCacheMatrix <- function(x = matrix()) {
-      x
+      m <- NULL
+      set <- function(y) {
+        x <<- y
+        m <<- NULL
+      }
+      # Get the matrix values
+      get <- function() 
+      # get the invers
+      setm <- function(solve) m <<- solve
+      getm <- function() m
+      # Return the matrix as a list
+      list(set = set, get = get,
+           setm = setm,
+           getm = getm)
 }
 
-# This function returns inverse of the a matrix if the determinant is different of zero
+# This function returns inverse of the matrix
 cacheSolve <- function(x, ...) {
-    # Check the determinant
-    if (det(x) != 0) {
-      inv <- solve(x)
+      # Get the matrix
+      inv <- x$getm()
+      # If is null, get the matrix in cache
+      if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+      }
+      # else, calculate the matrix
+      data <- x$get()
+      inv <- solve(data, ...)
+      x$setm(inv)
+      # Return the inverse
       inv
-    }
-    else {
-      # Can't calculate the inverse
-      print("This matriz is not invertible")
-    }
 }
